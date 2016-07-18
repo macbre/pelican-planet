0fee57dbec4dc0554561016d2fac7950526e6ca1
Mathieu Bridon <bochecha@daitauha.fr>
2016-07-18 17:06:45 +0200

    Initial project skeleton

---
+++ README.md
@@ -0,0 +1,41 @@
+# Blog aggregation, static-website style!
+
+This is a plugin for the [Pelican](https://getpelican.com) static site
+generator.
+
+It allows generating a page aggregating blog articles from other web sites.
+
+## Usage
+
+Install this plugin:
+
+    $ pip install git+https://framagit.org/bochecha/pelican-planet#egg=pelican_planet
+
+Then, in your Pelican config file, enable the plugin:
+
+    PLUGINS = [
+        ...
+        'pelican_planet',
+        ...
+        ]
+
+**TODO:** Document this.
+
+## Template design
+
+**TODO:** Document this.
+
+## Optional configuration
+
+**TODO:** Document this.
+
+## Legalities
+
+pelican-planet is offered under the terms of the
+[GNU Affero General Public License, either version 3 or any later version](http://www.gnu.org/licenses/agpl.html).
+
+We will never ask you to sign a copyright assignment or any other kind of
+silly and tedious legal document before accepting your contributions.
+
+In case you're wondering, we do **not** consider that a website built with
+pelican-planet would need to be licensed under the AGPL.
---
+++ setup.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2016 - Mathieu Bridon <bochecha@daitauha.fr>
+#
+# This file is part of pelican-planet
+#
+# pelican-planet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pelican-planet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with pelican-planet.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+
+def get_requirements(path):
+    lines = Path(path).open('r')
+    lines = map(lambda l: l.strip(), lines)
+    lines = filter(lambda l: bool(l), lines)
+    lines = filter(lambda l: not l.startswith('#'), lines)
+
+    return list(lines)
+
+
+README = Path('README.md').open().read()
+CHANGES = Path('CHANGES.md').open().read()
+REQUIREMENTS = get_requirements('requirements.txt')
+
+
+setup(
+    name='pelican-planet',
+    description='Blog aggregator plugin for Pelican',
+    long_description='%s\n\n%s' % (README, CHANGES),
+    version='0.0',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        ('License :: OSI Approved :: GNU Affero General Public License v3 or '
+         'later (AGPLv3+)'),
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        ],
+    author='Mathieu Bridon',
+    author_email='bochecha@daitauha.fr',
+    url='',  # TODO
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    )
