7027c6e0123ac586ee95d95401f7eeeba9b7b9e3
Maciej Brencz <maciej.brencz@gmail.com>
2019-02-19 18:17:21 +0100

    Add a missing import

--- pelican_planet/planet.py
+++ pelican_planet/planet.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with pelican-planet.  If not, see <http://www.gnu.org/licenses/>.
 import logging
+import re
 
 from operator import attrgetter
 
